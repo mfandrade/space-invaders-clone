51651ae85bd1a57cf2de1c91f4e1e9769faabcd6
Marcelo F Andrade <mfandrade@gmail.com>
2023-09-18 11:12:18 -0300

    understanding invaders positioning

--- spaceinvaders.js
+++ spaceinvaders.js
@@ -7,14 +7,17 @@ for (let i = 0; i < 225; i++) {
 
 const squares = Array.from(document.querySelectorAll('#grid div'));
 
-const aliens = [
+const alienPos = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
     15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39
 ];
 
 function draw() {
-    for (let i = 0; i < aliens.length; i++) {
-        squares[aliens[i]].classList.add('invader');
+    for (let i = 0; i < alienPos.length; i++) {
+        //console.log(`i:${i} alienPos:${alienPos[i]}`)
+        //squares[alienPos[i]].innerHTML = i;
+        squares[alienPos[i]].classList.add('invader');
     }
 }
+draw();

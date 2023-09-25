d4b183ce91e4fcb9ae3bc6a4e1cad6dadb4765dd
Marcelo F Andrade <mfandrade@gmail.com>
2023-09-25 11:59:42 -0300

    refactor and moveShooter function

--- game.js
+++ game.js
@@ -1,29 +1,44 @@
-const grid = document.querySelector('#grid');
+const screen = document.querySelector('#grid');
 
 const TILES_PER_ROW = 15;
 
-let spaceshipPos = ((TILES_PER_ROW-1) * TILES_PER_ROW) - (Math.round(TILES_PER_ROW/2));
+let shooterPos = ((TILES_PER_ROW-1) * TILES_PER_ROW) - (Math.round(TILES_PER_ROW/2));
 
 
 for (let i = 0; i < TILES_PER_ROW ** 2; i++) {
-    const square = document.createElement('div');
-    grid.appendChild(square);
+    const tile = document.createElement('div');
+    screen.appendChild(tile);
 }
 
-const squares = Array.from(document.querySelectorAll('#grid div'));
+const tiles = Array.from(document.querySelectorAll('#grid div'));
 
-const alienPos = [
+const invadersPos = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
     15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39
 ];
 
 function draw() {
-    for (let i = 0; i < alienPos.length; i++) {
-        squares[alienPos[i]].classList.add('invader');
+    for (let i = 0; i < invadersPos.length; i++) {
+        tiles[invadersPos[i]].classList.add('invader');
     }
 }
 draw();
 
-squares[spaceshipPos].classList.add('shooter');
+tiles[shooterPos].classList.add('shooter');
+
+function moveShooter(e) {
+    tiles[shooterPos].classList.remove('shooter');
+
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (shooterPos % TILES_PER_ROW > 0) --shooterPos;
+            break;
+        case 'ArrowRight':
+            if (shooterPos % TILES_PER_ROW < TILES_PER_ROW-1) ++shooterPos;
+            break;
+    }
+    tiles[shooterPos].classList.add('shooter');
+}
+document.addEventListener('keydown', moveShooter);
 
